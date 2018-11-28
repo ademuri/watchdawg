82cd58d85f3ae1b893d04a7643a3081d6d5d66f7
Adam Demuri <adam.demuri@gmail.com>
2018-11-27 22:43:08 -0700

    Log when would have rebooted

--- watchdawg.py
+++ watchdawg.py
@@ -2,7 +2,11 @@ import urllib2
 import logging
 import time
 
-def has_internet():
+INTERNET_CHECK_DELAY = 60
+INTERNET_CHECK_CYCLES_TO_BAD = 10
+CAN_LOAD_PAGE_RETRY_DELAY = 5
+
+def can_load_page():
     try:
         urllib2.urlopen('https://google.com', timeout=5)
         return True
@@ -10,14 +14,27 @@ def has_internet():
         return False
 
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-
-while True:
-    i = has_internet()
+def has_internet():
+    i = can_load_page()
     if not i:
         logging.info('connectivity check failed, trying again')
-        time.sleep(5)
-        i = has_internet()
+        time.sleep(CAN_LOAD_PAGE_RETRY_DELAY)
+        i = can_load_page()
+    return i
 
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+no_internet_cycles = 0
+while True:
+    i = has_internet()
     logging.info('has internet: %d', i)
-    time.sleep(60)
+    if i:
+        no_internet_cycles = 0
+    else:
+        no_internet_cycles += 1
+        if no_internet_cycles > INTERNET_CHECK_CYCLES_TO_BAD:
+            logging.info('would have rebooted')
+            no_internet_cycles = 0
+
+    time.sleep(INTERNET_CHECK_DELAY)
