14ed290782474a97b5e1bd90d1132c8f0b375ea0
Adam Demuri <adam.demuri@gmail.com>
2018-12-02 14:32:12 -0700

    Add local connectivity check and fix params

--- watchdawg.py
+++ watchdawg.py
@@ -1,22 +1,27 @@
 from urllib.request import urlopen
-from urllib.error import URLError
+from urllib.error import URLError, HTTPError
 import logging
 import time
 from datetime import datetime, timedelta
 import argparse
 import json
 
-"""
 INTERNET_CHECK_DELAY = 60
 INTERNET_CHECK_CYCLES_TO_BAD = 10
-CAN_LOAD_PAGE_RETRY_DELAY = 5
-MIN_TIME_BETWEEN_REBOOTS = timedelta(hours = 1)
-"""
+CAN_LOAD_PAGE_RETRY_DELAY = 10
 
+MIN_TIME_BETWEEN_REBOOTS = timedelta(hours = 1)
+INTERNET_URL = 'https://google.com'
+LOCAL_URL = 'http://192.168.1.1'
+
+"""
 INTERNET_CHECK_DELAY = 5
 INTERNET_CHECK_CYCLES_TO_BAD = 1
 CAN_LOAD_PAGE_RETRY_DELAY = 1
 MIN_TIME_BETWEEN_REBOOTS = timedelta(seconds = 20)
+INTERNET_URL = 'https://google.com'
+LOCAL_URL = 'http://192.168.88.1'
+"""
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
 
@@ -27,7 +32,6 @@ parser.add_argument('--dry_run', dest='dry_run', action='store_true', help='only
 parser.add_argument('--nodry_run', dest='dry_run', action='store_false', help='actually perform actions')
 parser.set_defaults(dry_run=True)
 args = parser.parse_args()
-print(args)
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
@@ -43,27 +47,43 @@ if args.file:
         logging.info("File doesn't exist (everything is normal)") 
 
 
-def can_load_page():
+def can_load_page(url):
     try:
-        urlopen('https://google.com', timeout=5)
+        urlopen(url, timeout=5)
         return True
     except URLError as err: 
         return False
+    except HTTPError as err:
+        if err.code == 401:
+            return True
+        else:
+            # TODO: does HTTPError always mean we have connectivity?
+            return False
 
 
 def has_internet():
-    i = can_load_page()
+    i = can_load_page(INTERNET_URL)
     if not i:
-        logging.info('connectivity check failed, trying again')
+        logging.info('internet connectivity check failed, trying again')
         time.sleep(CAN_LOAD_PAGE_RETRY_DELAY)
-        i = can_load_page()
+        i = can_load_page(INTERNET_URL)
+    return i
+
+
+def has_local_network():
+    i = can_load_page(LOCAL_URL)
+    if not i:
+        logging.info('local connectivity check failed, trying again')
+        time.sleep(CAN_LOAD_PAGE_RETRY_DELAY)
+        i = can_load_page(LOCAL_URL)
     return i
 
 
 no_internet_cycles = 0
 while True:
     i = has_internet()
-    logging.info('has internet: %d', i)
+    l = has_local_network()
+    logging.info('has internet: %d, has local network: %d', i, l)
     if i:
         no_internet_cycles = 0
     else:
