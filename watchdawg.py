e141866351143e39d629d7cde01edcd2ae86636d
Adam Demuri <adam.demuri@gmail.com>
2018-12-02 14:20:11 -0700

    Add hysteresis and dry run flag

--- watchdawg.py
+++ watchdawg.py
@@ -1,16 +1,53 @@
-import urllib2
+from urllib.request import urlopen
+from urllib.error import URLError
 import logging
 import time
+from datetime import datetime, timedelta
+import argparse
+import json
 
+"""
 INTERNET_CHECK_DELAY = 60
 INTERNET_CHECK_CYCLES_TO_BAD = 10
 CAN_LOAD_PAGE_RETRY_DELAY = 5
+MIN_TIME_BETWEEN_REBOOTS = timedelta(hours = 1)
+"""
+
+INTERNET_CHECK_DELAY = 5
+INTERNET_CHECK_CYCLES_TO_BAD = 1
+CAN_LOAD_PAGE_RETRY_DELAY = 1
+MIN_TIME_BETWEEN_REBOOTS = timedelta(seconds = 20)
+
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
+parser = argparse.ArgumentParser(description='Checks for connectivity')
+parser.add_argument('--file', type=str, help='file for saving hysteresis data')
+# See https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+parser.add_argument('--dry_run', dest='dry_run', action='store_true', help='only log')
+parser.add_argument('--nodry_run', dest='dry_run', action='store_false', help='actually perform actions')
+parser.set_defaults(dry_run=True)
+args = parser.parse_args()
+print(args)
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+last_rebooted = datetime.min
+if args.file:
+    try:
+        with open(args.file) as f:
+            saved_data = json.load(f)
+            if saved_data['last_rebooted']:
+                last_rebooted = datetime.strptime(saved_data['last_rebooted'], DATE_FORMAT)
+                logging.info('Loaded last rebooted data: %s', last_rebooted)
+    except FileNotFoundError:
+        logging.info("File doesn't exist (everything is normal)") 
+
 
 def can_load_page():
     try:
-        urllib2.urlopen('https://google.com', timeout=5)
+        urlopen('https://google.com', timeout=5)
         return True
-    except urllib2.URLError as err: 
+    except URLError as err: 
         return False
 
 
@@ -23,8 +60,6 @@ def has_internet():
     return i
 
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-
 no_internet_cycles = 0
 while True:
     i = has_internet()
@@ -34,7 +69,17 @@ while True:
     else:
         no_internet_cycles += 1
         if no_internet_cycles > INTERNET_CHECK_CYCLES_TO_BAD:
-            logging.info('would have rebooted')
-            no_internet_cycles = 0
+            if (datetime.now() - last_rebooted) < MIN_TIME_BETWEEN_REBOOTS:
+                logging.info('Would have rebooted, but rate-limited')
+            else:
+                no_internet_cycles = 0
+                if args.dry_run:
+                    logging.warning('would have rebooted')
+                    last_rebooted = datetime.now()
+                else:
+                    logging.warning('rebooting')
+                    with open(args.file, 'w') as f:
+                        f.write(json.dumps({'last_rebooted': datetime.now().strftime(DATE_FORMAT)}))
+                    # TODO: actually reboot
 
     time.sleep(INTERNET_CHECK_DELAY)
